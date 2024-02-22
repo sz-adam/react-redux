8dd7c3c5ebbed0e3fc21c4046a2e46988db304db
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-22 18:56:41 +0100

    add redux cart visible

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,17 @@
-import classes from './CartButton.module.css';
+import { useDispatch } from "react-redux";
+
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  
+  const toggleCarthandler = () => {
+    dispatch(uiActions.toogle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCarthandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
